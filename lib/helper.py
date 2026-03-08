659844c59b9d39fac9cd3499e81f12785ba4fa21
Adrian Chen <absszero@gmail.com>
2026-03-08 10:52:15 +0800

    minor changes

--- lib/helper.py
+++ lib/helper.py
@@ -5,8 +5,7 @@ from .place import Place
 class Helper:
     def get_place(self, path, line, lines=''):
         pattern = compile(r"""([\w^_]+)_path\(\s*(['"])([^'"]*)\2""")
-        matched = pattern.search(line) or\
-            pattern.search(lines)
+        matched = pattern.search(line) or pattern.search(lines)
         if (matched and path == matched.group(3)):
             prefix = matched.group(1) + '/'
             if 'base/' == prefix:
@@ -16,9 +15,9 @@ class Helper:
 
             return Place(prefix + path)
 
+        # to_action
         pattern = compile(r"""to_action\(\s*\[([^,]+),\s*(['"])([^'"]*)\2""")
-        matched = pattern.search(line) or\
-            pattern.search(lines)
+        matched = pattern.search(line) or pattern.search(lines)
         if (matched and path == matched.group(3)):
             return Place(matched.group(1).replace('::class', '').replace('\\', '/') + '.php@' + matched.group(3))
 
